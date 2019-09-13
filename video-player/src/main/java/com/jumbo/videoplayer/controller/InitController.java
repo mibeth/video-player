039c3c87759182d4be1fd9dad9064b02173c0cfb
mibeth <mibeth@gmail.com>
2019-09-14 00:08:45 +0200

    Adding server-port

--- video-player/src/main/java/com/jumbo/videoplayer/controller/InitController.java
+++ video-player/src/main/java/com/jumbo/videoplayer/controller/InitController.java
@@ -4,15 +4,16 @@ package com.jumbo.videoplayer.controller;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
 
 @Controller
 public class TestController {
     @Value("${spring.application.name}")
     String appName;
 
-    @RequestMapping("/idle")
+    @GetMapping("/idle")
     public String homePage(Model model) {
         model.addAttribute("appName", appName);
         return "index";
