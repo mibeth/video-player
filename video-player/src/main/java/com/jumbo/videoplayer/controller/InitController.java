5eb9d17449722e30a89280ee92269f11eb594019
mibeth <mibeth@gmail.com>
2019-09-14 10:27:21 +0200

    size video Player

--- video-player/src/main/java/com/jumbo/videoplayer/controller/InitController.java
+++ video-player/src/main/java/com/jumbo/videoplayer/controller/InitController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 
 @Controller
-public class TestController {
+public class InitController {
     @Value("${spring.application.name}")
     String appName;
 
